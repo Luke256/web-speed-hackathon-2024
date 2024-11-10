92d1d7b1cae8748f09dfe0cdc0ce3a84614e5c1e
Luke256 <yuta.k.200628@gmail.com>
2024-11-10 11:18:16 +0900

    optimize author page

--- workspaces/app/src/features/book/components/BookListItem.tsx
+++ workspaces/app/src/features/book/components/BookListItem.tsx
@@ -27,12 +27,21 @@ const _ImgWrapper = styled.div`
   }
 `;
 
-type Props = {
-  bookId: string;
+type Book = {
+  id: string;
+  name: string;
+  description: string;
+  image: {
+    id: string;
+  };
 };
 
-export const BookListItem: React.FC<Props> = ({ bookId }) => {
-  const { data: book } = useBook({ params: { bookId } });
+type Props = {
+  book: Book;
+};
+
+export const BookListItem: React.FC<Props> = ({ book }) => {
+  // const { data: book } = useBook({ params: { bookId } });
 
   const imageUrl = useImage({ height: 64, imageId: book.image.id, width: 64 });
 
